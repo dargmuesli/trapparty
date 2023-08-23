069ab4e25ea93a615fb2479a8536c97e348d5070
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-23 07:08:01 +0200

    test(e2e): expect page to be loaded

--- src/tests/e2e/specs/pages/root/privacy-policy.spec.ts
+++ src/tests/e2e/specs/pages/root/privacy-policy.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-import { COOKIE_CONTROL_DEFAULT } from '../../../utils/constants'
+import { COOKIE_CONTROL_DEFAULT, PAGE_READY } from '../../../utils/constants'
 
 test.beforeEach(async ({ context }) => {
   await context.addCookies([
@@ -23,11 +23,7 @@ test.describe('page load', () => {
 test.describe('visual regression', () => {
   test('looks as before', async ({ page }) => {
     await page.goto('/privacy-policy')
-    await expect(page.getByTestId('is-loading')).toHaveAttribute(
-      'data-is-loading',
-      'false',
-    )
-    await page.getByRole('button', { name: 'Cookie control' }).isVisible()
+    await PAGE_READY({ page })
     await expect(page).toHaveScreenshot({ fullPage: true })
   })
 })
