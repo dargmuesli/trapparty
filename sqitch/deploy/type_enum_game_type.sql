310e32ff8d37901c803e456735b11aa2fc1ddd92
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-12-18 11:33:26 +0100

    feat(game): add type for numeric estimation

--- sqitch/deploy/type_enum_game_type.sql
+++ sqitch/deploy/type_enum_game_type.sql
@@ -1,10 +1,11 @@
 BEGIN;
 
 CREATE TYPE trapparty.game_type AS ENUM (
+  'estimation_numeric',
   'random_facts',
   'team_score'
 );
 
-COMMENT ON TYPE trapparty.game_type IS 'Available game types: random_facts, team_score.';
+COMMENT ON TYPE trapparty.game_type IS 'Available game types: estimation_numeric, random_facts, team_score.';
 
 COMMIT;
