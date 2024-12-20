a9116d923c91369a0fecf25532ede4e1bd338f2f
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-12-20 15:15:03 +0100

    chore(urql): update plugin script

--- src/plugins/urql.ts
+++ src/plugins/urql.ts
@@ -6,9 +6,9 @@ import {
   type ClientOptions,
   type SSRData,
 } from '@urql/core'
-// import type { Data } from '@urql/exchange-graphcache'
 import {
-  /* Cache, */ offlineExchange as getOfflineExchange,
+  // type Cache,
+  offlineExchange as getOfflineExchange,
 } from '@urql/exchange-graphcache'
 import { makeDefaultStorage } from '@urql/exchange-graphcache/default-storage'
 // import { relayPagination } from '@urql/exchange-graphcache/extras'
@@ -16,24 +16,16 @@ import { devtoolsExchange } from '@urql/devtools'
 import { provideClient } from '@urql/vue'
 import type { Client } from '@urql/vue'
 import { consola } from 'consola'
-import type { Pinia } from 'pinia'
 import { ref } from 'vue'
 
 import schema from '~/gql/generated/introspection'
 // import type { GraphCacheConfig } from '~/gql/generated/graphcache'
-// import { cacheExchange } from '@urql/exchange-graphcache'
-
-// import {
-//   authenticationAnonymous,
-//   getJwtFromCookie,
-//   jwtRefresh,
-// } from '~/utils/auth'
 
 const SSR_KEY = '__URQL_DATA__'
 // const invalidateCache = (
 //   cache: Cache,
 //   name: string,
-//   args?: { input: { id: any } }
+//   args?: { input: { id: string | number | null } },
 // ) =>
 //   args
 //     ? cache.invalidate({ __typename: name, id: args.input.id })
@@ -43,36 +35,8 @@ const SSR_KEY = '__URQL_DATA__'
 //         .forEach((field) => {
 //           cache.invalidate('Query', field.fieldKey)
 //         })
-// TODO: create manual updates that do not require invalidation (https://github.com/maevsi/maevsi/issues/720)
-// const listPush = (cache: Cache, fieldName: string, data: Data | null) =>
-//   cache
-//     .inspectFields('Query')
-//     .filter((field) => field.fieldName === fieldName)
-//     .forEach((field) => {
-//       const dataField = cache.resolve('Query', field.fieldKey)
 
-//       if (typeof dataField !== 'string')
-//         throw new Error('Data field is no string!')
-
-//       const allInvitations = cache.resolve(dataField, 'nodes')
-
-//       if (
-//         !data ||
-//         !Array.isArray(allInvitations) ||
-//         !isNonEmptyArrayOfStrings(allInvitations)
-//       )
-//         throw new Error('Data field is no array!')
-
-// // TODO: insert IDs, not data  (https://github.com/maevsi/maevsi/issues/720)
-//       allInvitations.push(data)
-//       cache.link('Query', field.fieldKey, allInvitations)
-//     })
-
-// const isNonEmptyArrayOfStrings = (value: unknown): value is (string | Data)[] => {
-//   return Array.isArray(value) && value.every((item) => typeof item === 'string')
-// }
-
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(async (nuxtApp) => {
   const runtimeConfig = useRuntimeConfig()
   const getServiceHref = useGetServiceHref()
 
@@ -92,42 +56,73 @@ export default defineNuxtPlugin((nuxtApp) => {
     })
   }
 
-  // const cacheConfig: GraphCacheConfig = {
+  // const graphCacheConfig: GraphCacheConfig = {
   //   schema,
-  //   // resolvers: {
-  //   //   Query: {
-  //   //     allContacts: relayPagination(),
-  //   //     allEvents: relayPagination(),
-  //   //     allInvitations: relayPagination(),
-  //   //     allUploads: relayPagination(),
-  //   //   },
-  //   // },
-  //   storage: makeDefaultStorage(),
-  //   // updates: {
-  //   //   Mutation: {
-  //   //     // create
-  //   //     createContact: (_parent, _args, cache, _info) =>
-  //   //       invalidateCache(cache, 'allContacts'),
-  //   //     createInvitation: (_parent, _args, cache, _info) =>
-  //   //       invalidateCache(cache, 'allInvitations'),
-  //   //     // TODO: create manual updates that do not require invalidation (https://github.com/maevsi/maevsi/issues/720)
-  //   //     // listPush(cache, 'allInvitations', parent.createInvitation),
+  //   resolvers: {
+  //     Query: {
+  //       allContacts: relayPagination(),
+  //       allEvents: relayPagination(),
+  //       allInvitations: relayPagination(),
+  //       allUploads: relayPagination(),
+  //     },
+  //   },
+  //   updates: {
+  //     Mutation: {
+  //       // create
+  //       createContact: (_result, _args, cache, _info) =>
+  //         invalidateCache(cache, 'allContacts'),
+  //       createInvitation: (_result, _args, cache, _info) =>
+  //         invalidateCache(cache, 'allInvitations'),
+  //       // TODO: create manual updates that do not require invalidation (https://github.com/maevsi/maevsi/issues/720)
+  //       // createInvitation: (result, args, cache, info) => {
+  //       //   cache.updateQuery(
+  //       //     {
+  //       //       query: allInvitationsQuery,
+  //       //       variables: {
+  //       //         eventId: args.input.invitation.eventId,
+  //       //         first: 10, // Replace with the appropriate value or keep this dynamic
+  //       //         after: null, // Update accordingly, this could be the first page of results
+  //       //       },
+  //       //     },
+  //       //     (data) => {
+  //       //       if (data?.allInvitations?.nodes) {
+  //       //         // Append the new invitation to the allInvitations array
+  //       //         return {
+  //       //           ...data,
+  //       //           allInvitations: {
+  //       //             ...data.allInvitations,
+  //       //             nodes: [
+  //       //               ...data.allInvitations.nodes,
+  //       //               ...(result.createInvitation?.invitation
+  //       //                 ? [result.createInvitation?.invitation]
+  //       //                 : []),
+  //       //             ],
+  //       //             totalCount: data.allInvitations.totalCount + 1,
+  //       //           },
+  //       //         }
+  //       //       } else {
+  //       //         return data
+  //       //       }
+  //       //     },
+  //       //   )
+  //       // },
 
-  //   //     // update
-  //   //     profilePictureSet: (_parent, _args, cache, _info) =>
-  //   //       invalidateCache(cache, 'profilePictureByUsername'),
+  //       // update
+  //       profilePictureSet: (_result, _args, cache, _info) =>
+  //         invalidateCache(cache, 'profilePictureByAccountId'),
 
-  //   //     // delete
-  //   //     deleteContactById: (_parent, args, cache, _info) =>
-  //   //       invalidateCache(cache, 'Contact', args),
-  //   //     deleteInvitationById: (_parent, args, cache, _info) =>
-  //   //       invalidateCache(cache, 'Invitation', args),
-  //   //   },
-  //   // },
+  //       // delete
+  //       deleteContactById: (_result, args, cache, _info) =>
+  //         invalidateCache(cache, 'Contact', args),
+  //       deleteInvitationById: (_result, args, cache, _info) =>
+  //         invalidateCache(cache, 'Invitation', args),
+  //     },
+  //   },
   // }
 
   const cacheExchange = import.meta.client
     ? getOfflineExchange({
+        // ...graphCacheConfig,
         schema,
         storage: makeDefaultStorage(),
       })
@@ -136,17 +131,21 @@ export default defineNuxtPlugin((nuxtApp) => {
   const clientOptions: ClientOptions = {
     requestPolicy: 'cache-and-network',
     fetchOptions: () => {
-      const { $pinia } = useNuxtApp()
-      const store = useVioAuthStore($pinia as Pinia) // TODO: remove typecast
+      const store = useVioAuthStore()
       const headers = {} as Record<string, string>
-      const jwt = store.jwt
 
-      if (jwt) {
-        consola.trace('GraphQL request authenticated with: ' + jwt)
-        headers.authorization = `Bearer ${jwt}`
+      if (store.jwt) {
+        consola.trace('GraphQL request authenticated with: ' + store.jwt)
+        headers.authorization = `Bearer ${store.jwt}`
       } else {
         consola.trace('GraphQL request without authentication.')
       }
+
+      // if (store.turnstileToken) {
+      //   consola.debug(`Turnstile token: ${store.turnstileToken}`)
+      //   headers[TURNSTILE_HEADER_KEY] = store.turnstileToken
+      //   store.turnstileToken = undefined
+      // }
 
       return { headers }
     },
@@ -156,7 +155,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     exchanges: [
       ...(runtimeConfig.public.vio.isInProduction ? [] : [devtoolsExchange]),
       ...(cacheExchange ? [cacheExchange] : []),
-      ssrExchange, // `ssr` must be before `fetchExchange`
+      ssrExchange, // `ssrExchange` must be before `fetchExchange`
       fetchExchange,
     ],
   }
@@ -170,26 +169,26 @@ export default defineNuxtPlugin((nuxtApp) => {
   })
 
   // // Either authenticate anonymously or refresh token on page load.
-  // if (nuxtApp.ssrContext?.event) {
-  //   const store = useStore(nuxtApp.ssrContext.$pinia)
-  //   const jwtFromCookie = getJwtFromCookie({
-  //     req: nuxtApp.ssrContext.event.node.req,
-  //   })
+  // if (nuxtApp.ssrContext) {
+  //   const store = useVioAuthStore()
+  //   const jwtFromCookie = getJwtFromCookie()
 
-  //   if (jwtFromCookie?.jwtDecoded.id) {
+  //   if (jwtFromCookie?.jwtDecoded?.id) {
   //     await jwtRefresh({
-  //       client: client.value,
   //       $urqlReset: urqlReset,
-  //       store,
-  //       res: nuxtApp.ssrContext.event.node.res,
+  //       client: client.value,
+  //       event: nuxtApp.ssrContext.event,
   //       id: jwtFromCookie.jwtDecoded.id as string,
+  //       isInProduction: runtimeConfig.public.vio.isInProduction,
+  //       store,
   //     })
   //   } else {
   //     await authenticationAnonymous({
-  //       client: client.value,
   //       $urqlReset: urqlReset,
+  //       client: client.value,
+  //       event: nuxtApp.ssrContext.event,
+  //       isInProduction: runtimeConfig.public.vio.isInProduction,
   //       store,
-  //       res: nuxtApp.ssrContext.event.node.res,
   //     })
   //   }
   // }
