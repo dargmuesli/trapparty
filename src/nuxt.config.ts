becd5267a171bbb6c4c8fb0208e3a152f7ff38ae
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-01-30 05:52:21 +0100

    refactor(nuxt): move configuration back up

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -5,7 +5,7 @@ import IconsResolver from 'unplugin-icons/resolver'
 import Icons from 'unplugin-icons/vite'
 import Components from 'unplugin-vue-components/vite'
 
-import { SITE_NAME, PRODUCTION_HOST } from '../shared/utils/constants'
+import { SITE_NAME, PRODUCTION_HOST } from './shared/utils/constants'
 
 export default defineNuxtConfig(
   defu(
