af4b25edf04782acd259ce6f03d20a6a5ece0778
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-07-08 01:33:53 +0200

    chore(test): update snapshots

--- tests/e2e/specs/pages/root/privacy-policy.spec.ts
+++ tests/e2e/specs/pages/root/privacy-policy.spec.ts
@@ -11,8 +11,8 @@ testA11y(PAGE_PATH)
 testPageLoad(PAGE_PATH)
 testOgImage({
   dynamic: {
-    de: 'a_TrapPartys+Logo.,c_Default.takumi,description_TrapPartys+Online-Version.,title_~RGF0ZW5zY2h1dHplcmtsw6RydW5n,p_Ii9kZS9wcml2YWN5LXBvbGljeSI.png',
-    en: `a_TrapParty's+logo.,c_Default.takumi,description_TrapParty's+online+version.,title_Privacy+Policy,p_Ii9wcml2YWN5LXBvbGljeSI.png`,
+    de: 'a_TrapPartys+Logo.,c_Default.takumi,description_TrapPartys+Online-Version.,title_~RGF0ZW5zY2h1dHplcmtsw6RydW5n,p_Ii9kZS9wcml2YWN5LXBvbGljeSI,s_pJ75Fqjag2EZ1hpa.png',
+    en: `a_TrapParty's+logo.,c_Default.takumi,description_TrapParty's+online+version.,title_Privacy+Policy,p_Ii9wcml2YWN5LXBvbGljeSI,s_zi765JjnMNoGlRW6.png`,
   },
 })
 testVisualRegression(PAGE_PATH)
