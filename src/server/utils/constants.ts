7b2ab975ca50df84fd24097a869d0d1c0524037e
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-08-27 13:07:25 +0200

    feat(sentry): use vio's built-in Sentry integration

--- src/server/utils/constants.ts
+++ src/server/utils/constants.ts
@@ -5,13 +5,27 @@ export const GET_CSP = ({ siteUrl }: { siteUrl: URL }) => {
   const domainTldPort = IS_IN_FRONTEND_DEVELOPMENT
     ? PRODUCTION_HOST
     : getRootHost(siteUrl.host)
+  const sentryConfig = useRuntimeConfig().public.sentry
 
-  return defu({
-    // TrapParty
-    'connect-src': [`https://trapparty-postgraphile.${domainTldPort}/graphql`],
-    'font-src': ["'self'"], // og-image
-    'report-to': 'csp-endpoint',
-    'report-uri':
-      'https://o4507259039973376.ingest.de.sentry.io/api/4507260561653840/security/?sentry_key=1e53178c1dba9b39147de4a21853a3e3',
-  })
+  return defu(
+    {
+      // TrapParty
+      'connect-src': [
+        `https://trapparty-postgraphile.${domainTldPort}/graphql`,
+      ],
+      'font-src': ["'self'"], // og-image
+      'report-to': 'sentry',
+    },
+    {
+      // Sentry (vio only wires this in automatically for static builds)
+      ...(sentryConfig.host && sentryConfig.project.id
+        ? {
+            'connect-src': [
+              `https://${sentryConfig.host}/api/${sentryConfig.project.id}/envelope/`,
+            ],
+            'worker-src': ['blob:'],
+          }
+        : {}),
+    },
+  )
 }
